9cbcce8aab983508486d7efcf7e27dc3b174a37d
unknown <jabravo19@iespuntadelverde,es>
2020-10-02 15:32:18 +0200

    primer ejercicio

---
+++ src/variables.ts
@@ -0,0 +1,15 @@
+// Usamos let y Const
+
+// En este caso crearemos 2 variables con let, y las uniremos en una constante
+// posteriormente esta constante se mostrará por consola
+
+let nombre= "Julio Andrés";
+let apellido= "Bravo Lozano";
+
+const alumno={ nombre, apellido},string = {
+    nombre: nombre,
+    apellido: apellido
+};
+
+console.log(alumno)
+
